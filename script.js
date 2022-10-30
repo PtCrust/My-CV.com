33d6f12480ec05b480441b53834f03ac16b22ece
Tony <61390876+PtCrust@users.noreply.github.com>
2022-10-30 16:45:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFun(){
+    document.getElementById("1stCert").height="500";
+    document.getElementById("1stCert").width="500";
+}
+
+function revert(){
+    document.getElementById("1stCert").height="100";
+    document.getElementById("1stCert").width="100";
+
+}
