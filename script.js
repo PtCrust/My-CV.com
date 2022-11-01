4da86199c64494b0e71cb47046679798d701f80a
Tony <61390876+PtCrust@users.noreply.github.com>
2022-11-01 18:56:16 +0200

    Add files via upload
    new

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function myFun(){
-    document.getElementById("1stCert").height="500";
-    document.getElementById("1stCert").width="500";
-}
 
-function revert(){
-    document.getElementById("1stCert").height="100";
-    document.getElementById("1stCert").width="100";
+var menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+function togglemenu(){
+    if(menuList.style.maxHeight == "0px")
+    {
+        menuList.style.maxHeight = "130px";
+    }
+    else{
+        menuList.style.maxHeight = "0px";
 
+    }
 }
